b5dcffca4eb539c0ad19d54ce9c51fedc5c54b50
Raffi <raffidarmawan0411@gmail.com>
2025-06-21 12:43:05 +0700

    added code

--- day-4-optional-review-score/src/main/kotlin/Main.kt
+++ day-4-optional-review-score/src/main/kotlin/Main.kt
@@ -1,6 +1,36 @@
 package org.example
 
-fun main() {
-    // Day 4 Kotlin Challenge: Optional Review Score
+data class Book (
+    val title: String,
+    val author: String,
+    val year: Int,
+    val rating: Double? = null
+)
 
+fun printBookReview(book: Book){
+    // Original Code
+//    book.let {
+//        println("Title: ${it.title} by ${it.author}")
+//        println("Review Score: ${it.rating ?: "N/A"}")
+//    }
+
+    // or
+
+    val reviewScore = book.rating?.let { rating ->
+        "${rating}/10.0"
+    } ?: "N/A"
+
+    println("Title: ${book.title} by ${book.author}")
+    println("Review Score: $reviewScore")
+}
+
+fun main() {
+    val bookWithReview = Book("1984", "George Orwell", 1949, 8.7)
+    val bookWithoutReview = Book("Animal Farm", "George Orwell", 1945)
+
+    println("--- Review 1 ---")
+    printBookReview(bookWithReview)
+
+    println("\n--- Review 2 ---")
+    printBookReview(bookWithoutReview)
 }
